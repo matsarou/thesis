9cd4725a45ba1fb2e84c54afcff7d533f8149a0a
matsarou <matsarou@hotmail.com>
2021-07-11 17:41:48 +0200

    grit computing method

--- grid_computing_method.py
+++ grid_computing_method.py
@@ -4,25 +4,32 @@ from scipy import stats
 
 # Taken from book:
 # Bayesian analysis with python, by Osvaldo Martin
-def posterior_grid_approx(grid_points, heads, tosses):
-    # 1. Define an interval for the parameter (the prior should give you a hint). --> grid_points
-    # 2. Place a grid of points on that interval. --> grid
-    # 3. For each point in the grid we multiply the likelihood and the prior.
-    # 4. Optionally, we may normalize the computed values
+def posterior_grid(grid_points=50, heads=6, tails=9):
+    """
+    A grid implementation for the coin-flipping problem
+    """
     grid = np.linspace(0, 1, grid_points)
-    prior = np.repeat(5, grid_points)
-    likelihood = stats.binom.pmf(heads, tosses, grid)
-    unstd_posterior = likelihood * prior
-    posterior = unstd_posterior / unstd_posterior.sum()
+    prior = np.repeat(1/grid_points, grid_points)  # uniform prior
+    likelihood = stats.binom.pmf(heads, heads+tails, grid)
+    posterior = likelihood * prior
+    posterior /= posterior.sum()
     return grid, posterior
 
 
 def main():
-    points = 15
-    h, n = 1, 4
-    grid, posterior = posterior_grid_approx(points, h, n)
-    plt.plot(grid, posterior, 'o-', label='heads = {}\ntosses = {}'.format(h, n))
+
+
+    data = np.repeat([0, 1], (10, 3))
+    points = 10
+    h = data.sum()
+    t = len(data) - h
+    grid, posterior = posterior_grid(points, h, t)
+    plt.plot(grid, posterior, 'o-')
+
+    plt.title(f'heads = {h}, tails = {t}')
+    # plt.yticks([])
     plt.xlabel(r'$\theta$')
+    plt.ylabel(r'posterior')
     plt.legend(loc=0)
     plt.show()
 
